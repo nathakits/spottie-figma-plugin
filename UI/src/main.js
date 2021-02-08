db638c04a40dd20e53fdd48a54810fde72fa5241
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-02-08 21:49:14 +0700

    import css in main.js

--- UI/src/main.js
+++ UI/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store';
+import "tailwindcss/tailwind.css"
+import "./assets/css/index.css"
 
 Vue.config.productionTip = false
 
