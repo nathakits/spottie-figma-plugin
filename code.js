57f6aca9084931170cc08259d77e62ebee84176f
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-02-12 02:06:50 +0700

    implement inserting spotify image to plugin - click to create rect and add img fill - or select an object to add img fill

--- code.js
+++ code.js
@@ -23,6 +23,59 @@ figma.ui.onmessage = msg => {
         figma.currentPage.selection = nodes;
         figma.viewport.scrollAndZoomIntoView(nodes);
     }
+    if (msg.type === 'notify') {
+        figma.notify(msg.message);
+    }
+    if (msg.type === 'create-image') {
+        const nodes = [];
+        // get current selection
+        var currentSel = figma.currentPage.selection;
+        // if selection has an object
+        if (currentSel.length === 1) {
+            // loop nodes to check type
+            currentSel.forEach(node => {
+                if (node.type === 'FRAME' ||
+                    node.type === 'ELLIPSE' ||
+                    node.type === 'POLYGON' ||
+                    node.type === 'RECTANGLE' ||
+                    node.type === 'STAR' ||
+                    node.type === 'VECTOR') {
+                    // inset fill to node
+                    var buffer = msg.buffer;
+                    var hash = figma.createImage(buffer).hash;
+                    node.fills = [
+                        { type: 'IMAGE', scaleMode: 'FILL', imageHash: hash }
+                    ];
+                }
+                else {
+                    figma.notify(`Please select a fillable object`);
+                }
+            });
+        }
+        // else create a new rectangle and add to page
+        else if (currentSel.length === 0) {
+            // image
+            var buffer = msg.buffer;
+            var hash = figma.createImage(buffer).hash;
+            // viewport
+            var viewport = figma.viewport.center;
+            // create rectangle and set image fill
+            const rect = figma.createRectangle();
+            rect.x = viewport.x;
+            rect.y = viewport.y;
+            rect.resize(800, 800);
+            rect.fills = [
+                { type: 'IMAGE', scaleMode: 'FILL', imageHash: hash }
+            ];
+            // add image to Figma
+            figma.currentPage.appendChild(rect);
+            nodes.push(rect);
+        }
+        // notify user
+        else {
+            figma.notify(`Please select one object`);
+        }
+    }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     // figma.closePlugin();
--- code.ts
+++ code.ts
@@ -14,24 +14,59 @@ figma.showUI(__html__, { width: 450, height: 600 });
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
+  if (msg.type === 'create-image') {
     const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
+    // get current selection
+    var currentSel = figma.currentPage.selection
+    // if selection has an object
+    if (currentSel.length === 1) {
+      // loop nodes to check type
+      currentSel.forEach(node => {
+        if (
+          node.type === 'FRAME' ||
+          node.type === 'ELLIPSE' ||
+          node.type === 'POLYGON' ||
+          node.type === 'RECTANGLE' ||
+          node.type === 'STAR' ||
+          node.type === 'VECTOR'
+        ) {
+          // inset fill to node
+          var buffer = msg.buffer
+          var hash = figma.createImage(buffer).hash
+          node.fills = [
+            { type: 'IMAGE', scaleMode: 'FILL', imageHash: hash }
+          ];
+        }
+        else {
+          figma.notify(`Please select a fillable object`)
+        }
+      });
+    } 
+    // else create a new rectangle and add to page
+    else if (currentSel.length === 0) {
+      // image
+      var buffer = msg.buffer
+      var hash = figma.createImage(buffer).hash
+      // viewport
+      var viewport = figma.viewport.center
+      // create rectangle and set image fill
       const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      // set x and y coordinates with viewport values
+      rect.x = viewport.x
+      rect.y = viewport.y
+      // hard code rect size
+      rect.resize(800, 800)
+      // set type to IMAGE and set fill with image hash data
+      rect.fills = [
+        { type: 'IMAGE', scaleMode: 'FILL', imageHash: hash }
+      ];
+      // add image to Figma
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-
-    if (msg.type === 'notify') {
-      figma.notify(msg.message)
+    // notify user
+    else {
+      figma.notify(`Please select one object`)
     }
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  // figma.closePlugin();
 };
