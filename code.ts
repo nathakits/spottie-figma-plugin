835c5ff0dc4789cb1c09242fb3499e8d8dea825d
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-02-13 16:16:53 +0700

    update build

--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 450, height: 600 });
+figma.showUI(__html__, { width: 450, height: 650 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
