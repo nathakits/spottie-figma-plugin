393ba27d93852d2fbaf79e3b5c05f978d13057e1
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-02-25 00:45:53 +0700

    update to 2 spacing

--- UI/vue.config.js
+++ UI/vue.config.js
@@ -5,20 +5,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const watch = process.argv.includes("--watch-inline") ? process.argv.includes("--watch-inline") : false
 
 module.exports = {
-    css: {
-        extract: true, // don't extract the internal css chunks to external file
+  css: {
+    extract: true, // don't extract the internal css chunks to external file
+  },
+  configureWebpack: {
+    watch, // watch the file changes and build the final UI that can used by Figma plugin directly
+    optimization: {
+      splitChunks: false // makes sure there only be 1 js file - leftover from earlier attempts but doesn't hurt
     },
-    configureWebpack: {
-        watch, // watch the file changes and build the final UI that can used by Figma plugin directly
-        optimization: {
-            splitChunks: false // makes sure there only be 1 js file - leftover from earlier attempts but doesn't hurt
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: 'public/index.html', // this is important - a template file to use for insertion
-                inlineSource: '.(js|css)$' // embed all javascript and css inline
-            }),
-            new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin), // this is important - inline the generated js to the index.html
-        ]
-    }
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html', // this is important - a template file to use for insertion
+        inlineSource: '.(js|css)$' // embed all javascript and css inline
+      }),
+      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin), // this is important - inline the generated js to the index.html
+    ]
+  }
 };
