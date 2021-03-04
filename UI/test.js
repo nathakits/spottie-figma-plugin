56870abfc0770cf80f20ca7d02e3ecb8161a7f6b
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-03-05 02:23:24 +0700

    add test file

---
+++ UI/test.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const qs = require('qs');
+require('dotenv').config()
+var data = qs.stringify({
+  'grant_type': 'client_credentials'
+});
+
+const auth = process.env.VUE_APP_SPOTIFY_AUTH_BASIC
+
+try {
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: data,
+    headers: {
+      'Authorization': `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  }).then(response => {
+    console.log(`Success`)
+  })
+} catch(error) {
+  console.log(error)
+}
